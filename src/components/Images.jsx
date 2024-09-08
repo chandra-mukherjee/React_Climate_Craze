bc533bc6e534785b577aeb6159567eb0c782728b
unknown <chandramukherjee2020@gmail.com>
2024-09-08 11:52:06 +0530

    first commit

---
+++ src/components/Images.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import clouds from '../Assets/weather/clouds.jpg'
+import mist from '../Assets/weather/mist.jpg'
+import clear from '../Assets/weather/clear.jpg'
+import drizzle from '../Assets/weather/drizzle.jpg'
+import snow from '../Assets/weather/snow.jpg'
+import rain from '../Assets/weather/rain.jpg'
+import thunderstorm from '../Assets/weather/thunderstorm.jpg'
+import haze from '../Assets/weather/haze.jpg'
+import nf from '../Assets/weather/nf.jpg'
+
+const Images = {
+    clouds,
+    mist,
+    clear,
+    drizzle,
+    snow,
+    rain,
+    thunderstorm,
+    haze,
+    nf
+}
+
+export default Images;
